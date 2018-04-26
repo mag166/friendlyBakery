48bc960a433746d1142e838278ce03ebe1f2ac9b
Minhal <mag166@case.edu>
2018-04-26 15:41:45 -0400

    final tweeks

--- app.rb
+++ app.rb
@@ -28,7 +28,37 @@ post "/" do
     puts params[:comment]
     puts ENV["SENDGRID"]
     subject = 'Bakery Catalog for ' + params[:name]
-    content = Content.new(type: 'text/html', value:"hello<br>goodbye" )
+    content = Content.new(type: 'text/html', value:"
+    <p><strong> Thank you for subscribing to our catalog! Below is a list of our products:</strong></p>
+    <p>Cakes(price per cake):</p>
+    <ul>
+        <li>Cheesecake - $20</li>
+        <li>Chocolate Cake - $25</li>
+        <li>Vanilla Cake - $20</li>
+        <li>Red Velvet Cake - $20</li>
+        <li>Strawberry Shortcake - $20</li>
+        <li>Carrot Cake - $18</li>
+    </ul>
+    <br>
+    <p>Cookies(price per dozen)</p>
+    <ul>
+        <li>Snickerdoodle - $15</li>
+        <li>Peanut Butter - $15</li>
+        <li>White Chocolate - $10</li>
+        <li>M&M - $15</li>
+        <li>Caramel - $10</li>
+        <li>Oatmeal - $15</li>
+    </ul>
+    <p>Muffins(price per dozen)</p>
+    <ul>
+        <li>Blueberry - $15</li>
+        <li>Coffee Cake - $20</li>
+        <li>Chocolate Chip - $15</li>
+        <li>Bran - $20</li>
+        <li>Double Chocolate - $20</li>
+        <li>Cranberry - $15</li>
+    </ul>
+    ")
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV["SENDGRID"])
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
 M.AutoInit();
+// var elem = document.querySelector('.dropdown-trigger');
+// var instance = M.Dropdown.init(elem, options)
 
-var elem = document.querySelector('.dropdown-trigger');
-var instance = M.Dropdown.init(elem, options)
 
+// var elem = document.querySelector('.parallax');
+// var instance = M.Parallax.init(elem, options);
 
-var elem = document.querySelector('.parallax');
-var instance = M.Parallax.init(elem, options);
+let button = document.getElementById("theButton")
+button.addEventListener('click', (event) => {
+    alert("Thanks for signing up!")
+})
