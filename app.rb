cd9b0c22bcd42f21ee9c478bf93ede49ae19041d
Minhal <mag166@case.edu>
2018-04-26 13:41:04 -0400

    a lot of images and stuff

--- app.rb
+++ app.rb
@@ -3,19 +3,32 @@ require 'sinatra/reloader'
 require 'sendgrid-ruby'
 include SendGrid
 
-from = Email.new(email: 'minhal.gardezi@gmail.com')
-to = Email.new(email: 'minhal.gardezi@gmail.com')
+
 
 
 get "/" do
     erb :index
 end
 
+get "/cakes" do
+    erb :cakes
+end
+
+get "/cookies" do
+    erb :cookies
+end
+
+get "/muffins" do
+    erb :muffins
+end
+
 post "/" do
+    from = Email.new(email: 'minhal@bakery.com')
+    to = Email.new(email: params[:email])
     puts params[:comment]
     puts ENV["SENDGRID"]
-    subject = 'Comment From ' + params[:name]
-    content = Content.new(type: 'text/plain', value: params[:comment])
+    subject = 'Bakery Catalog for ' + params[:name]
+    content = Content.new(type: 'text/html', value:"hello<br>goodbye" )
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV["SENDGRID"])
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+M.AutoInit();
+
+var elem = document.querySelector('.dropdown-trigger');
+var instance = M.Dropdown.init(elem, options)
+
+
+var elem = document.querySelector('.parallax');
+var instance = M.Parallax.init(elem, options);
